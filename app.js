fa0a576605a346ff9890d72997fa403e62d0e477
Syed Asif Uddin <syed.uddin@lsz.ads.valuelabs.net>
2019-02-27 12:20:39 +0530

    Game Completed"

--- app.js
+++ app.js
@@ -9,58 +9,56 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-document.querySelector('.dice').style.display = 'none';
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-  // 1. Random Number
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if(gamePlaying){
 
-  //2. Display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png'
-
-  //3. Update the round score IF the rolled number was NOT a 1
-  if(dice !== 1){
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    //next player
-    nextPlayer();
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+  
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+  
+    //3. Update the round score IF the rolled number was NOT a 1
+    if(dice !== 1){
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //next player
+      nextPlayer();
+    }
   }
 
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  //Add current score to Global Score
-  scores[activePlayer] += roundScore;
+  if(gamePlaying){
 
+    //Add current score to Global Score
+    scores[activePlayer] += roundScore;
   
-  //Update the Ui
-  document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
-
-
-  //Check if player won the game
-  if(scores[activePlayer] >= 10){
-    document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    nextPlayer();
+    
+    //Update the Ui
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+  
+  
+    //Check if player won the game
+    if(scores[activePlayer] >= 10){
+      document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
 
 });
@@ -78,3 +76,29 @@ function nextPlayer(){
 
   diceDOM.style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.querySelector('#name-0').textContent = 'Player 1';document.querySelector('#name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+
+}
+
+
